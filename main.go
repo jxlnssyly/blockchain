323ff644681f1b16dd522c7c1eae6648a0d17784
Liaoy <363918226@qq.com>
2019-06-03 14:09:18 +0800

    02_实现SetHash

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"crypto/sha256"
+)
+
+// 定义结构
+type Block struct {
+	// 前区块哈希
+	PrevHash []byte
+
+	// 当前区块哈希
+	Hash []byte
+
+	// 数据
+	Data []byte
+}
+
+// 创建区块
+func NewBlock(data string, prevBlockHash []byte) *Block  {
+	block := Block{
+		PrevHash:prevBlockHash,
+		Hash: []byte{}, // TODO 修改
+		Data: []byte(data),
+	}
+	block.SetHash()
+	return &block
+}
+
+// 3.生成哈希
+func (block *Block) SetHash() {
+	// 1.拼装数据
+	blockInfo := append(block.PrevHash,block.Data...) // byte拼接
+	// 2.sha256
+
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
+
+
+func main() {
+
+	block := NewBlock("老师转班长一枚比特币", []byte{})
+	fmt.Printf("前区块哈希: %x\n",block.PrevHash)
+	fmt.Printf("当前区块哈希: %x\n",block.Hash)
+	fmt.Printf("区块数据: %s\n",block.Data)
+}
